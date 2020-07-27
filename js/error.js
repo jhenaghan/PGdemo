1a8ae01fa6470919c6761ada04fa0ced5fc7ef3e
jhenaghan <62388193+jhenaghan@users.noreply.github.com>
2020-07-26 21:22:40 -0500

    Update error.js

--- js/error.js
+++ js/error.js
@@ -59,7 +59,7 @@ $('#error-button').on('click', function() {
  //   email = $('#inputEmail').val();
 
     var alertbox = `
-	<div id="alert" class="alert alert-danger alert-dismissible fade show" role="alert" text-align: center>
+	<div id="alert" class="alert alert-danger alert-dismissible fade show" role="alert">
 		<button type="button" class="close" data-dismiss="alert" aria-label="Close">
 			<span aria-hidden="true">&times;</span>
 		</button>
